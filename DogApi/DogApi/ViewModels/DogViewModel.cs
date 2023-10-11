bc34da0bd8af12039e2872769339f6bb5413221d
darhan1168 <darinahanina04@gmail.com>
2023-10-11 21:49:04 +0200

    Create DogViewModel.cs

---
+++ DogApi/DogApi/ViewModels/DogViewModel.cs
@@ -0,0 +1,12 @@
+namespace DogApi.ViewModels;
+
+public class DogViewModel
+{
+    public string Name { get; set; }
+    
+    public string Color { get; set; }
+    
+    public decimal TailLength { get; set; }
+
+    public decimal Weight { get; set; }
+}
